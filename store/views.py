2d7dcc069f0dccb4a652cd12c44c59f45ed95ff7
Memory-Wabwile <memorywabwile@gmail.com>
2022-06-23 21:13:20 +0300

    create the carts view

--- store/views.py
+++ store/views.py
@@ -7,3 +7,10 @@ def store(request):
     message = "Thisis the stores page"
 
     return request (request , 'store/store.html' , {"message" : message})
+
+def cart(request):
+
+    message = "Thisis the cart page"
+
+    return request (request , 'store/cart.html' , {"message" : message})
+
