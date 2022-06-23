e78ffe8de0a1ffa80ef624f39c2a7028aac11a52
Memory-Wabwile <memorywabwile@gmail.com>
2022-06-23 21:12:20 +0300

    create the view for the stores template

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
+from email import message
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+
+    message = "Thisis the stores page"
+
+    return request (request , 'store/store.html' , {"message" : message})
