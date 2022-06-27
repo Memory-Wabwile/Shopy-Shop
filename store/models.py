8fdaf57f4231f71113e100db9ee3a606662364f8
Memory-Wabwile <memorywabwile@gmail.com>
2022-06-27 13:09:00 +0300

    add function to handle error incase there is no image uploaded

--- store/models.py
+++ store/models.py
@@ -21,6 +21,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+# handling error incase there is no image
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer , on_delete=models.SET_NULL , blank=True , null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
