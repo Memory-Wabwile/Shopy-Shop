7413b6e1ac25cd0af17d20b662e66b9a5564f552
Memory-Wabwile <memorywabwile@gmail.com>
2022-06-27 10:45:30 +0300

    create the models for the order

--- store/models.py
+++ store/models.py
@@ -15,6 +15,14 @@ class Product(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default=False , null=True , blank=False)
 
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
-    customer = models.
+    customer = models.ForeignKey(Customer , on_delete=models.SET_NULL , blank=True , null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False , null=True)
+    transaction_id = models.CharField(max_length=200 , null=True)
+
+    def __str__(self):
+        return str(self.id)
