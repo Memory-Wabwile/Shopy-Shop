93a815ee2dbf6ff9fe8f905e4d8ee0dbeab9185a
Memory-Wabwile <memorywabwile@gmail.com>
2022-07-08 17:49:03 +0300

    ensure the logic for both the authenticated and non authenticated users works

--- store/views.py
+++ store/views.py
@@ -148,22 +148,9 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order ,  created = Order.objects.get_or_create(customer = customer , complete=False)
-        total =float(data['form']['total'])
-        order.transaction_id = transaction_id
+       
 
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer = customer,
-                order = order,
-                address = data['shipping']['address'],
-                city = data['shipping']['city'],
-                state = data['shipping']['state'],
-                zipcode = data['shipping']['zipcode'],
-            )
+       
     else:
         print("user is not logged in")
 
@@ -189,4 +176,26 @@ def processOrder(request):
         for item in items:
             product = Product.objects.get(id=item['product']['id'])
 
+            OrderItem = OrderItem.objects.create(
+                product = product,
+                order = order,
+                quanity = item['quanity'],
+            )
+
+    total =float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+        order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+            )  
     return JsonResponse('Payment Complete' , safe=False)
