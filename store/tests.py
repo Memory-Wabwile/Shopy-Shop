46de4452cf4f5f9a1992bf4cf8b38d66862effca
Memory-Wabwile <94103821+Memory-Wabwile@users.noreply.github.com>
2025-12-03 10:48:54 +0300

    update tests.py
    update tests.py

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase , Client
+from store.models import Product
+from django.urls import reverse
+from store.models import Product
 
-# Create your tests here.
+class StoreViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        Product.objects.create(name="Test Product", price=100)
+
+    def test_homepage_loads(self):
+        response = self.client.get(reverse("store"))
+        self.assertEqual(response.status_code, 200)
+      
+class ProductModelTest(TestCase):
+    def test_create_product(self):
+        product = Product.objects.create(
+            name="Test Product",
+            price=100,
+            description="Just a test"
+        )
+        self.assertEqual(product.name, "Test Product")
+        self.assertEqual(product.price, 100)
+
+
+
+
+
+
